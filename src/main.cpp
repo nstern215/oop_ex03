f84011f4fe2517a3bdab93c03eef17ac48df3120
Netanel Stern <netanel@ezsave.co.il>
2021-11-23 22:52:13 +0200

    add testing

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,31 @@
 
 int main()
 {
-	Poly p1(Rational(1));
+	/*Poly p1(Rational(1));
 	Poly p2;
 
 	std::cout << (p1 == p2) << std::endl;
-	std::cout << (p1 != p2) << std::endl;
+	std::cout << (p1 != p2) << std::endl;*/
 
-	std::vector<Rational> poly = {(1,2), (2,3), (3,4), (4,5)};
+	/*std::vector<Rational> poly = {(1,2), (2,3), (3,4), (4,5)};*/
 
-	Poly p3(poly), p4(poly);
+	/*Poly p3(poly), p4(poly);*/
 
-	std::cout << (p3 == p4) << std::endl;
+	std::vector<Rational> poly = {
+		Rational(1,2),
+		Rational(2),
+		Rational(1,5),
+		Rational(5)
+	};
+	//std::vector<Rational> poly;
+	//poly.push_back(Rational(1, 2));
+	//poly.push_back(Rational(2));
+	//poly.push_back(Rational(1, 5));
+	//poly.push_back(Rational(5));
+	
+	Poly p(poly);
+	
+	std::cout << p(2) << std::endl;
 	
     return 0;
 }
